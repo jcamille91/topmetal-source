67bb6a21343a54985b246ac89867d37208e09092
Joseph Camilleri <josephcamilleri@Josephs-MacBook.local>
2016-11-09 16:50:05 -0800

    modifying the tm1x1 branch to read dmux. currently dmux'ing, but not sure if the output is correct yet. baseline level and noise level looks okay, not sure about the pulses though.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# topmetal-source
+# topmetal-source TM-1x1 branch (starting nov 9 2016)
 
-wow VERY impressive edits, well done
+The 1x8 detector ADC is faulty, so going to configure the software for some older 1x1 sensor data.
+
+changes: 
+
+1. this detector's ADC has a different topology, so the ADC dataformat is a signed int, as opposed to the 1x8 ADC's unsigned int. This will require small changes in the hdf5rawWaveformIo source and header files.
+
+2. The ADC waveform attribute arrays for converting to volts from samples have 16 elements for the 1x1 file, instead of the 8 element arrays, so account for this. These attributes are yoff, yzero, and ymult.
+
+-> change SCOPE_NCH to 16, this should fix the arrays cleanly in the hdf5 read_waveform_attribute_in_file_header function.
+
+3. The onboard memory used for this data acquisition was different than the 1x8 detector.
+
+-> change SCOPE_MEM_LENGTH_MAX
+
--- common.h
+++ common.h
@@ -6,11 +6,28 @@
 
 #define HDF5IO(name) hdf5io_ ## name
 
-#define SCOPE_NCH 8
-#define SCOPE_MEM_LENGTH_MAX 16777216 /* 256MiB memory, (16-bit X 8-ch) per point */
-#define SCOPE_DATA_TYPE_INT uint16_t
+/* filling memory per channel:
+
+1x8 sensor: 256 MB available. shared by 8ch. each point is 2 bytes (unsigned).
+so nPt = 256*(2^20)/(8*2) = 16777216
+
+1x16 sensor: 1GB available. shared by 16ch. each point is 2 bytes (signed)
+so nPt = 1*(2^30)/(16*2) = 33554432
+
+the 1x1 sensor uses the entire 1GB storage for the one active channel, 
+but still has SCOPE_NCH set to 16. so use the nPt of the combined 16 channels for a single
+channel, but still set SCOPE_NCH to get the waveform attribute format correct so we can read the
+data out.
+
+so nPt = 16 * 33554432 = 536870912
+*/
+
+#define SCOPE_NCH 16
+#define SCOPE_MEM_LENGTH_MAX 33554432 // GiB memory, (16-bit x 1-ch) per point
+// #define SCOPE_MEM_LENGTH_MAX 16777216 // 256MiB memory, (16-bit X 8-ch) per point
+#define SCOPE_DATA_TYPE_INT int16_t
 #define SCOPE_DATA_TYPE_FLOAT float
-#define SCOPE_DATA_HDF5_TYPE_INT H5T_NATIVE_UINT16
+#define SCOPE_DATA_HDF5_TYPE_INT H5T_NATIVE_INT16
 #define SCOPE_DATA_HDF5_TYPE_FLOAT H5T_NATIVE_FLOAT
 
 
